e4336e833bd2b5ae98a0511cd4c33e312f76e7da
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-04-27 22:31:48 +0200

    Poprawienie headera

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Button } from "@mui/material";
+
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography variant="h6" component={Link} to="/">
-          Team Creator
-        </Typography>
-        <Button component={Link} to="/teams">Teams</Button>
+    <AppBar position="static" color="primary">
+      <Toolbar sx={{ display: "flex", gap: "16px" }}>
+        <Button component={Link} to="/" variant="contained" size="large">
+          Players
+        </Button>
+        <Button component={Link} to="/teams" variant="contained" size="large">
+          Teams
+        </Button>
       </Toolbar>
     </AppBar>
   );
