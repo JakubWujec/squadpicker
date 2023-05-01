7df4ac8a045853e312c4d74305c2ef1b67b39d5f
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-05-01 22:29:16 +0200

    usePlayers hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
 import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AddPlayers from './AddPlayers';
-import Header from './Header';
-import TeamSelection from './TeamSelection';
-import { Player } from './types';
-import { useState, useEffect } from 'react';
-import TeamCompatibilities from './TeamCompatibilities';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-function getLocalStorage<T>(key: string, initialValue: T) {
-  try {
-    const value = window.localStorage.getItem(key);
-    return value ? JSON.parse(value) : initialValue;
-  } catch (e) {
-    // if error, return initial value
-    return initialValue;
-  }
-}
+import AddPlayers from './AddPlayers';
+import Header from './Header';
+import TeamCompatibilities from './TeamCompatibilities';
+import TeamSelection from './TeamSelection';
+import { usePlayers } from './hooks/usePlayers';
 
 function App() {
-  const [players, setPlayers] = useState<Player[]>(() => getLocalStorage<Player[]>("players", []));
-
-  useEffect(() => {
-    const storedPlayers = localStorage.getItem("players");
-    if (storedPlayers) {
-      const parsedPlayers = JSON.parse(storedPlayers) as Player[];
-      setPlayers(parsedPlayers);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('players', JSON.stringify(players));
-  }, [players]);
-
-  function addPlayer(player: Player) {
-    setPlayers((players) => [...players, player])
-  }
-
-  function removePlayer(playerId: number) {
-    setPlayers(players => players.filter(p => p.id != playerId))
-  }
+  const { players, addPlayer, removePlayer } = usePlayers();
 
   return (
     <div>
---
+++ src/hooks/usePlayers.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Player } from '../types';
+import { toast } from 'react-toastify';
+
+export function usePlayers() {
+  const [players, setPlayers] = useState<Player[]>(() =>
+    getLocalStorage<Player[]>('players', [])
+  );
+
+  useEffect(() => {
+    localStorage.setItem('players', JSON.stringify(players));
+  }, [players]);
+
+  function addPlayer(newPlayer: Player) {
+    if (players.some(player => player.name === newPlayer.name)) {
+      toast("Already exist")
+    } else {
+      setPlayers((players) => [...players, newPlayer]);
+    }
+
+  }
+
+  function removePlayer(playerId: number) {
+    setPlayers((players) => players.filter((p) => p.id !== playerId));
+  }
+
+  return { players, addPlayer, removePlayer };
+}
+
+function getLocalStorage<T>(key: string, initialValue: T) {
+  try {
+    const value = window.localStorage.getItem(key);
+    return value ? JSON.parse(value) : initialValue;
+  } catch (e) {
+    // if error, return initial value
+    return initialValue;
+  }
+}
+
+
