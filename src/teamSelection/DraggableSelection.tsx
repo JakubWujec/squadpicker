93a7d6b4f69848cb9b06af249f76259e39932eae
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-05-20 20:30:17 +0200

    WIP

--- src/teamSelection/DraggableSelection.tsx
+++ src/teamSelection/DraggableSelection.tsx
@@ -1,6 +1,6 @@
 
 
-import { DragDropContext } from '@hello-pangea/dnd';
+import { DragDropContext, OnDragEndResponder } from '@hello-pangea/dnd';
 import { Box } from '@mui/material';
 import { Player } from '../types';
 import DroppableTeamColumn from './DroppableTeamColumn';
@@ -10,8 +10,20 @@ interface DraggableSelectionProps {
 
 const DraggableSelection = ({ teams }: DraggableSelectionProps) => {
 
-  const dragEndHandler = ({ source, destination }) => {
-    console.log(source, destination)
+  const dragEndHandler: OnDragEndResponder = ({ source, destination }) => {
+    console.log(source, destination);
+    swapPlayers(teams[0][0], teams[1][0])
+  }
+
+  const swapPlayers = (player1: Player, player2: Player) => {
+    if (teams[0].includes(player1) && teams[1].includes(player2)) {
+      teams[0].splice(0, 1, player2)
+      teams[1].splice(0, 1, player1)
+    }
+    if (teams[0].includes(player2) && teams[1].includes(player1)) {
+      teams[0].splice(0, 1, player1)
+      teams[1].splice(0, 1, player2)
+    }
   }
 
   return (
